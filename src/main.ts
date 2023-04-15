ef654e58cc9b4488515e1a003e851c658ee1b95f
admin <bonjorim@gmail.com>
2023-04-15 23:11:03 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { JWTPayloadType } from './types and models/types';
 import { createApp } from './helpers/createApp';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const rawApp = await NestFactory.create(AppModule);
   const app = createApp(rawApp);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
 
