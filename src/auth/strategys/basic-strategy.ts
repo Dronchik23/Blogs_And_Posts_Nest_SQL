07ab35576c28533a69212060f136c74763ce6b9e
admin <bonjorim@gmail.com>
2023-05-04 18:48:25 +0300

    test

--- src/auth/strategys/basic-strategy.ts
+++ src/auth/strategys/basic-strategy.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class BasicAuthGuard extends AuthGuard('basic') {}
+//export class BasicAuthGuard extends AuthGuard('basic') {}
+export class BasicAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    //TODO ?
+    const creds = request.headers['authorization']?.split(' ')[1];
+    if (creds === 'YWRtaW46cXdlcnR5') {
+      return true;
+    }
+    return false;
+  }
+}
