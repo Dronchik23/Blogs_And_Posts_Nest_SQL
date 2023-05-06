fb35947a639b1d58fd79b384cc9719c90c47151a
admin <bonjorim@gmail.com>
2023-05-06 13:09:54 +0300

    test

--- src/query-repositorys/blogs-query.repository.ts
+++ src/query-repositorys/blogs-query.repository.ts
@@ -69,7 +69,12 @@ export class BlogsQueryRepository {
       `
 SELECT * FROM blogs
 WHERE (name ILIKE '%' || $1 || '%' OR $1 IS NULL) AND "blogOwnerId" = $2 AND $2 NOT IN (SELECT id FROM users WHERE "isBanned" = true)
-ORDER BY "${sortBy}" ${sortDirection}
+ORDER BY 
+  CASE 
+    WHEN SUBSTRING(name from 1 for 1) = LOWER(SUBSTRING(name from 1 for 1)) THEN LOWER(name) 
+    ELSE name 
+  END 
+  ${sortDirection}
 LIMIT $3
 OFFSET $4;
 `,
