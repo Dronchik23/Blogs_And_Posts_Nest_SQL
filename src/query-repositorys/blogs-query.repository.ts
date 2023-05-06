470160e0511364ac95dcb33bbb3d3205c8d02b3a
admin <bonjorim@gmail.com>
2023-05-06 13:59:29 +0300

    test

--- src/query-repositorys/blogs-query.repository.ts
+++ src/query-repositorys/blogs-query.repository.ts
@@ -109,8 +109,8 @@ WHERE (name ILIKE '%' || $1 || '%' OR $1 IS NULL) AND "blogOwnerId" = $2 AND $2 
     const blogs: BlogDBType[] = await this.dataSource.query(
       `
   SELECT * FROM blogs
-  WHERE (name ILIKE $1 OR $1 IS NULL)
-  ORDER BY "${sortBy}" "${sortDirection}"
+  WHERE (name ILIKE $1 COLLATE "C" OR $1 IS NULL)
+  ORDER BY "${sortBy}" COLLATE "C" "${sortDirection}"
   LIMIT $2
   OFFSET $3;
 `,
